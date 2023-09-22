c3cbedb8bcdaa73b276b0137a43962ae48f618ff
Vladimir <quilmar@yandex.ru>
2023-09-22 11:33:53 +0300

    #2

---
+++ #2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Наибольшим числом из {numberA} и {numberB} является {numberA}.");
+}
+else
+{
+    Console.WriteLine($"Наибольшим числом из {numberA} и {numberB} является {numberB}.");
+}
